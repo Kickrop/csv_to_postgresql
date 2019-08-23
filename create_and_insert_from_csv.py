709b765b34203cc4ab415b57b3db2fe245fcceeb
martynov <kickrop@gmail.com>
2019-08-23 17:11:22 +0300

    fixed delimetr bug

--- create_and_insert_from_csv.py
+++ create_and_insert_from_csv.py
@@ -8,21 +8,22 @@ conn = psycopg2.connect(host=private.sr_host, dbname=private.sr_dbname, user=pri
 cur = conn.cursor()
 
 #specify postgres schema
-schema = 'stat_customs' #'fronts'
+schema = 'fronts' #'stat_customs' #'fronts'
 
 #table name in postgres
-table_name = 'spr_cntr' #'cleaned_fronts_aug2019'
+table_name = 'fronts_aug19' #'cleaned_fronts_aug2019'
 
 #file that contains data to insert into postgres
-file_name = 'country' + '.csv'
-file_delimiter = ';'
+file_name = 'IndicatorsExport 08.19' + '.csv'
+file_delimiter = ";"
 
 #path to data file
-path = 'H:/Работа2/30.01.2019.Для Сагиевой/04.2019.РасчетЭкспорта' #'H:/Fronts/08_2019'
+path = 'H:/Fronts/08_2019' #'H:/Работа2/30.01.2019.Для Сагиевой/04.2019.РасчетЭкспорта' #'H:/Fronts/08_2019'
 
 os.chdir(path)
 
 def create_table_with_csvheader():
+    #make shure headers inside csv file doesn't have spaces
     with open(file_name, 'r', encoding="utf8") as f:
         csv_reader = csv.reader(f, delimiter=file_delimiter)
         csv_header = next(csv_reader)
@@ -43,7 +44,7 @@ def insert_into_table():
     print('Insert may take some time...')
     with open(file_name, 'r', encoding="utf8") as f:
         #csv_reader = csv.reader(f, delimiter=file_delimiter)
-        cur.copy_expert(f"""COPY {schema}.{table_name} FROM STDIN WITH CSV HEADER DELIMITER as {file_delimiter}""", f) #sep=file_delimiter
+        cur.copy_expert(f"""COPY {schema}.{table_name} FROM STDIN WITH CSV HEADER DELIMITER as '{file_delimiter}'""", f)
     conn.commit()
     f.close()
     print('Inserted successfully')
