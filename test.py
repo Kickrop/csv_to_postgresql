dff165f9d65b0b4860f2bba68b137c61b570840e
martynov <kickrop@gmail.com>
2019-09-02 15:07:19 +0300

    cleaned up

--- test.py
+++ test.py
@@ -1,50 +1,44 @@
 import pandas as pd
-papers = pd.read_csv('unique15.csv')
-#for row in papers['accession_number']:
-#    print('UT=', row)
+papers = pd.read_csv('unique15.csv', converters={'accession_number': lambda x: str(x)})
+#добавляем UT= для каждого номера статьи
 papers['new_an'] = 'UT=' + papers['accession_number'].astype(str)
-group=''
-#group1=''
-# pam = []
-# for i in range(len(papers['new_an'])):
-#     if i%500==0:
-#         #group.append(i)
-#         pam.append(i)
-# pam.append(len(papers['new_an']))
-# print(pam)
-groups = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000,
-           6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, 11806]
 
+#формируем группы по 500 статей (для удобства скачивания из WOS)
+groups = []
+for i in range(len(papers['new_an'])):
+    if i%500==0:
+        #group.append(i)
+        groups.append(i)
+groups.append(len(papers['new_an']))
 
+#переводим строки в список
 papers_list = papers['new_an'].tolist()
+#groups = [0, 500, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000, 5500, 6000,
+#           6500, 7000, 7500, 8000, 8500, 9000, 9500, 10000, 10500, 11000, 11500, len(papers_list)]
 
-
+#формируем списки номеров статей через or, по <500шт.
+group=''
 for gr in groups:
     if gr+500 < len(papers_list):
         for i in range(gr, gr+500):
-            # if i < gr:
-            #     group= group + papers_list[i] + ' or '
-            # if i == gr:
-            #     group = group + papers_list[i] + ';'
             try:
                 if i < gr+500-2:
                     group = group + papers_list[i] + ' or '
                 if i == gr+500-1:
-                    group = group + papers_list[i] + ','
+                    group = group + papers_list[i] + '\n\n'
             except:
-                print('mistakes were made')
+                print('mistakes were made ', papers_list[i])
     else:
         for j in range(gr, len(papers_list)):
             if j < len(papers_list)-2:
                 group = group + papers_list[j] + ' or '
             if j == len(papers_list)-1:
                 group = group + papers_list[j]
-            # if i > gr and i < gr+500:
-            #     group = group + papers_list[i] + ' or '
-            # if i == gr+500:
-            #     group = group + papers_list[i] + ';'
-with open('output_test.txt', 'w') as f:
+
+#создаем текстовый файл, в который записываем сформированные группы со списками номеров статей                
+with open('new_papers_queries.txt', 'w') as f:
     f.write(group)
 f.close()
-#print(group[-1])
-#group.to_csv('pam.csv')
+print('done')
+#print(papers.head())
+
